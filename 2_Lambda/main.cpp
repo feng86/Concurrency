6cef7da66262601f5931476bd2e0fcc1f9e4e1ce
mfeng <mfeng@panyi.ai>
2021-01-13 13:09:35 +0800

    thread-based programming

--- 2_Lambda/main.cpp
+++ 2_Lambda/main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
+    // define inline anonymous function on the spot
+    //  lambda is more intuitive than global function for std::thread object 
+    //   just do it in parallel !
+    //   differnece between thread-based programming and  task-based programming 
     std::thread t([]()
     {
         std::cout << "Hello from thread!\n";
