9865ed02460603f99b67868b56804afc5b20fcdc
mfeng <mfeng@panyi.ai>
2021-01-14 09:32:06 +0800

    functor in std::thread

--- 5_FunctionObject/main.cpp
+++ 5_FunctionObject/main.cpp
@@ -7,9 +7,9 @@ class FunObj
 public:
     FunObj(int i) : _i(i)
     {}
-    void operator()()
+    void operator()(int j)
     {
-        std::cout << "Hello from thread " << _i << "!\n";
+        std::cout << "Hello from thread " << _i << "--" << j << " !\n";
     }
 private:
     int _i;
@@ -17,10 +17,14 @@ private:
 
 int main()
 {
+    // run a functor in a std::thread
+    //  functor can wrap data in a class/struct type
+    //  arguments can be passed in functor,no need for std::thread
+    //  no arguments at all if '2' is part of class/struct and passed in ctor
     std::vector<std::thread> threads;
     for (int i = 0; i < 16; ++i)
     {
-        threads.emplace_back(FunObj(i));
+        threads.emplace_back(FunObj(i),2);
     }
     std::cout << "Hello from main!\n";
     for (auto & t: threads)
