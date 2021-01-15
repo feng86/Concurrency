cdb284bd960413be10ca8957833ffbf34142f819
mfeng <mfeng@panyi.ai>
2021-01-15 09:32:35 +0800

    may fix reference to local var && many threads reference to same local var(shared memory)

--- 4_Bug/main.cpp
+++ 4_Bug/main.cpp
@@ -21,6 +21,9 @@ std::vector<std::thread> spawn()
             std::cout << "Hello from thread " << i << "!\n";
         });
     }
+    // i's lifetime is shorter than thread function which refers to it.
+    //  may fix: move join() here to halt in here to extend i's lifetime which is longer than thread function
+    //   but not always practically
     return threads;
 }
 
@@ -30,6 +33,8 @@ int main()
 #if 1
     for (int i = 0; i < 16; ++i)
     {
+	// big problem: all 16 threads refer to same variable i here(data structure e.g list) ==> shared memory
+        //  different thread runtime scheduling makes different thread sees different transient variable value(i) 
         threads.emplace_back([&]()
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(10 * i));
