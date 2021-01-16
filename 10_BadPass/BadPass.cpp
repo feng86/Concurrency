62c294f5d45192d4cf39ec8a2adf7ee39e841bda
mfeng <mfeng@panyi.ai>
2021-01-16 14:15:58 +0800

    normal return nothing,but need fut.get() to force normal function be called

--- 10_BadPass/BadPass.cpp
+++ 10_BadPass/BadPass.cpp
@@ -1,14 +1,23 @@
 #include <thread>
 #include <future>
 #include <memory>
+#include <iostream>
+#include <chrono>
 
+// no value is returned from this normal function
+//  you can still call fut.get(),when you call fut.get(), it call the function and it "extract future",but return nothing
 void threadFun(std::unique_ptr<int> p)
-{}
+{
+  std::cout << "start task.\n";
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "end task.\n";
+}
 
 int main()
 {
     std::unique_ptr<int> up(new int(10));
-    std::async(threadFun, std::move(up));
+    auto fut = std::async(threadFun, std::move(up));
+    fut.get(); //just called,no lhs is involved; it forces function to be called !!!
 
     return 0;
 }
